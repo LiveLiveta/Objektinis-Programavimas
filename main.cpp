3b6e0fddf539970480ab8f058da577da5d7c9b7a
Liveta Ruskute <ruskutes@gmail.com>
2025-02-10 14:44:58 +0200

    pridetos vidurkio ir medianos skaiciavimo funkcijos, pradetas duomenu printinimas

--- main.cpp
+++ main.cpp
@@ -9,19 +9,25 @@
 
     vector<studentas> Studentai;
 
-    void studento_duomenu_gavimas(vector<studentas> Studentai);
+    void studento_duomenu_gavimas(vector<studentas> &Studentai);
+    void studento_duomenu_printinimas(vector<studentas> Studentai);
+    double studento_vidurkio_skaiciavimas(vector<int> pazymiai, int egzamino_pazymys);
+    double studento_medianos_skaiciavimas(vector<int> pazymiai, int egzamino_pazymys);
 
 int main(){
+    studento_duomenu_gavimas(Studentai);
+    studento_duomenu_printinimas(Studentai);
     return 0;
 }
 
-void studento_duomenu_gavimas(vector<studentas> Studentai){
+void studento_duomenu_gavimas(vector<studentas> &Studentai){
+    while(true){
     studentas laikinas_studentas;
     cout << "Ivesite savo pavarde ir varda (Jei norite baigti irasyma rasykite 'n'.) :" << endl;
     cin >> laikinas_studentas.pavarde;
     if (laikinas_studentas.pavarde == "n"){
         return;
-    }
+    } else{
     cin >> laikinas_studentas.vardas;
     cout << "Iveskite kiek pazymiu turi studentas: " <<endl;
     int pazymiu_kiekis;
@@ -36,4 +42,37 @@ void studento_duomenu_gavimas(vector<studentas> Studentai){
     cout << "Iveskite egzamino pazymi: " <<endl;
     cin >> laikinas_studentas.egzamino_pazymys;
     Studentai.push_back(laikinas_studentas);
+    }
+    }
+}
+void studento_duomenu_printinimas(vector<studentas> Studentai){
+    cout << setw(13) << left << "Pavarde" << setw(12) << left << "Vardas"<< setw(12) << left << "Galutinis" << endl;
+    cout << "----------------------------------" << endl;
+    for (int i = 0; i < Studentai.size(); i ++){
+        cout << setw(13) << left << Studentai[i].pavarde << setw(12) << left << Studentai[i].vardas << setw(12) << left << fixed << setprecision(2) << (studento_vidurkio_skaiciavimas(Studentai[i].pazymiai, Studentai[i].egzamino_pazymys)) << endl;
+    } 
+}
+double studento_vidurkio_skaiciavimas(vector<int> pazymiai, int egzamino_pazymys){
+    int pazymiu_suma = 0;
+    int pazymiu_kiekis = pazymiai.size();
+
+    for (int i = 0; i < pazymiu_kiekis; i++){
+        pazymiu_suma += pazymiai[i];
+    }
+    double pazymiu_vidurkis = pazymiu_suma / pazymiu_kiekis;
+    double vidurkis = 0.4 * pazymiu_vidurkis + 0.6 * egzamino_pazymys;
+    return vidurkis;
+}
+double studento_medianos_skaiciavimas(vector<int> pazymiai, int egzamino_pazymys){
+    int pazymiu_kiekis = pazymiai.size();
+    sort(pazymiai.begin(), pazymiai.end());
+    double mediana;
+    if(pazymiu_kiekis%2 == 0){
+        mediana = (pazymiai[pazymiu_kiekis/2] + pazymiai[pazymiu_kiekis/2 -1]) / 2;
+    } else{
+        mediana = mediana = (pazymiai[pazymiu_kiekis/2]);
+    }
+
+    double vidurkis = 0.4 * mediana + 0.6 * egzamino_pazymys;
+    return vidurkis;
 }
--- pagalbines.h
+++ pagalbines.h
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using std:: cout;
 using std:: cin;
 using std:: endl;
 using std:: string;
 using std:: vector;
+using std:: setw;
+using std:: left;
+using std:: right;
+using std:: setprecision;
+using std:: fixed;
+using std:: sort;
