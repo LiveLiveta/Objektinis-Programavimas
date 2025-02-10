2ba24664fac7c235c1d30fa94079a978da0334d7
Liveta Ruskute <ruskutes@gmail.com>
2025-02-10 13:08:28 +0200

    Gaunami studento duomenys

--- main.cpp
+++ main.cpp
@@ -7,7 +7,33 @@
         int egzamino_pazymys;
     };
 
-int main(){
+    vector<studentas> Studentai;
 
+    void studento_duomenu_gavimas(vector<studentas> Studentai);
+
+int main(){
     return 0;
+}
+
+void studento_duomenu_gavimas(vector<studentas> Studentai){
+    studentas laikinas_studentas;
+    cout << "Ivesite savo pavarde ir varda (Jei norite baigti irasyma rasykite 'n'.) :" << endl;
+    cin >> laikinas_studentas.pavarde;
+    if (laikinas_studentas.pavarde == "n"){
+        return;
+    }
+    cin >> laikinas_studentas.vardas;
+    cout << "Iveskite kiek pazymiu turi studentas: " <<endl;
+    int pazymiu_kiekis;
+    cin >> pazymiu_kiekis;
+    cout << "Iveskite pazymius: "<<endl;
+
+    for(int i = 0;  i < pazymiu_kiekis; i++){
+        int pazymys;
+        cin >> pazymys;
+        laikinas_studentas.pazymiai.push_back(pazymys);
+    }
+    cout << "Iveskite egzamino pazymi: " <<endl;
+    cin >> laikinas_studentas.egzamino_pazymys;
+    Studentai.push_back(laikinas_studentas);
 }
