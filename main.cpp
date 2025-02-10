69bd15d61de863309948cfdd8d765e86bab3096a
Liveta Ruskute <ruskutes@gmail.com>
2025-02-10 11:53:15 +0200

    Struktura cpp faile

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "pagalbines.h"
+
+    struct studentas {
+        string pavarde;
+        string vardas;
+        vector<int> pazymiai;
+        int egzamino_pazymys;
+    };
+
+int main(){
+
+    return 0;
+}
