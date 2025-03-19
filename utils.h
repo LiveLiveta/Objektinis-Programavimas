219a15e5f7baeadb6d26d252b10bf36233dba907
Liveta Ruskute <ruskutes@gmail.com>
2025-03-20 01:13:36 +0200

    pasalinti nenaudojami failai

--- utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include "pagalbines.h"
-
-void formatReadmeTable(double** allTestsTimes, int numberOfRows, int numberOfColumns, int strategyNumber, string& containerType);
-int getNumberOfStudents(int index);
-
-#endif
